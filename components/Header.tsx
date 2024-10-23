a8561fb17e2d71d57d02f27fe0700444b6b47434
mazode <mrmaazkhan191@gmail.com>
2024-10-23 19:04:12 +0500

    styled the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavButton from './NavButton'
+import { Bars3BottomRightIcon } from "@heroicons/react/24/solid";
+
 
 function Header() {
   return (
-    <div>
+    <header className='grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5' >
       <div className='flex items-center space-x-2'>
         <img className='rounded-full h-20 w-20 ' src="/logo.jpeg" alt="" />
         <div>
@@ -11,13 +13,21 @@ function Header() {
             <p className='text-xs text-emerald-500 truncate'>User...</p>
         </div>
       </div> 
-      <div>
-        <div className='bg-[#0A1F1C]'>
+
+      <div className='hideen md:flex md:col-span-3 items-center justify-center rounded-md'>
+        <div className='bg-[#0A1F1C] p-4 space-x-2'>
             <NavButton isActive title='Buy Tickets'/>
             <NavButton title='Logout'/>
         </div>
       </div>
-    </div>
+
+      <div className='flex flex-col ml-auto text-right'>
+        <Bars3BottomRightIcon  className='h-8 w-8 mx-auto text-white cursor-pointer'/>
+        <span className='md:hidden'>
+          <NavButton title='Logout'/>
+        </span>
+      </div>
+    </header>
   )
 }
 
--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -6,7 +6,13 @@ interface Props {
 }
 
 function NavButton({ title, isActive }: Props) {
-  return <button className={`${isActive && "bg-[#036756]"} hover:bg-[#036756] text-white py-2 px-4 rounded`}> {title} </button>
+  return (
+    <button className={`${isActive && "bg-[#036756]"} hover:bg-[#036756] text-white py-2 
+      px-4 rounded font-bold`}
+    > 
+      {title} 
+    </button>
+  );
 }
 
 export default NavButton
