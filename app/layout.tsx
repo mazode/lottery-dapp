b02762fc0bd4406d71763398a099c6fa31257d5a
mazode <mrmaazkhan191@gmail.com>
2024-10-24 17:54:38 +0500

    added thirdweb sdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
+import { client } from "../lib/thirdwebClient";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThirdwebProvider>
+          {children}
+        </ThirdwebProvider>
       </body>
     </html>
   );
